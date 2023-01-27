5343ef712ee3cc10bcc3ad35d7f73698f09cbea5
armangelo <122386155+armangelo@users.noreply.github.com>
2023-01-27 19:44:33 +0100

    Ejercicio1

---
+++ JAVA/Ejercicio1/index.js
@@ -0,0 +1,10 @@
+console.log('hola');
+
+const volumen = 16 * 23 * 13;
+console.log('volumen', volumen + 'cm3');
+
+const areaLateral = 23 * 13;
+const areaBase = 16 * 23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log('areaTotal' + areaTotal + 'cm2');
+
