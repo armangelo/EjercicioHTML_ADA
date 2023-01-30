3932bfb1da41ff63dc6979934c03fcaee0b319e0
armangelo <122386155+armangelo@users.noreply.github.com>
2023-01-30 10:20:22 +0100

    Ejercicio15

---
+++ JAVA/Ejercicio15/index.js
@@ -0,0 +1,7 @@
+const users = [
+    {username: 'ppc90', age: 30, premium: false},
+    {username: 'lu65', age: 24, premium: false},
+    {username: 'maria3', age: 36, premium: false},
+    {username: 'abc123', age: 20, premium: true},
+    {username: 'sergio58', age: 26, premium: true},
+]
