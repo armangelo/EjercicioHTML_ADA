4696f084bbad4e4c67236937126f7cd07729f041
armangelo <122386155+armangelo@users.noreply.github.com>
2023-02-02 17:31:21 +0100

    Ejercicio17

--- JAVA/Ejercicio17/index.js
+++ JAVA/Ejercicio17/index.js
@@ -2,7 +2,7 @@
 let enlaces = document.getElementsByTagName("a");
 let numEnlaces = enlaces.length;
 let penultimoEnlace = enlaces[numEnlaces - 2];
-let tercerParrafo = document.getElementById(third-paragraph);
+let tercerParrafo = document.getElementById('third-paragraph');
 let numEnlacesTercerParrafo = tercerParrafo.getElementsByTagName("a").length;
 
 let resultados = document.createElement("p");
